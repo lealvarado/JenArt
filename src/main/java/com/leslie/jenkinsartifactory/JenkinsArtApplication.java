11a2de58711101908aa4874b8eee78441c5357f8
Leslie Alvarado <lesliemalvarado@gmail.com>
2018-12-21 16:31:14 -0500

    add in project

---
+++ src/main/java/com/leslie/jenkinsartifactory/JenkinsArtApplication.java
@@ -0,0 +1,14 @@
+package com.leslie.jenkinsartifactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsArtApplication.class, args);
+	}
+
+}
+
